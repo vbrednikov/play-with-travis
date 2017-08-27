ab15c516dce26a27fdb8e83b72d7fff3e84f5529
Vladimir Brednikov <vbrednikov@gmail.com>
2017-08-28 01:56:12 +0300

    break tests again

--- test.py
+++ test.py
@@ -5,5 +5,8 @@ class NumbersTest(unittest.TestCase):
     def test_equal(self):
         self.assertEqual(1 + 1, 2)
 
+    def test_not_equal(self):
+        self.assertNotEqual(1 + 1, 2)
+
 if __name__ == '__main__':
     unittest.main()
